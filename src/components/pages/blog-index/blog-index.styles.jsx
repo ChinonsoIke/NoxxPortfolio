b26450c4842e9d0014b4c5a6570ef7e70b3546d9
Chinonso Ikewelugo <chinonsoikewelugo@gmail.com>
2022-11-08 05:33:20 +0100

    fix blog index responsiveness

--- src/components/pages/blog-index/blog-index.styles.jsx
+++ src/components/pages/blog-index/blog-index.styles.jsx
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const BlogIndexStyles = styled.div`
     display: flex;
     padding: 0 5rem;
+
+    @media only screen and (max-width: 760px){
+        padding: 0 2rem;
+    }
 `
