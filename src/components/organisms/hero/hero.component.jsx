9514423003b0460558d4d4d4ed37681112531400
Chinonso Ikewelugo <chinonsoikewelugo@gmail.com>
2025-06-13 02:18:09 -0500

    punctuation

--- src/components/organisms/hero/hero.component.jsx
+++ src/components/organisms/hero/hero.component.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
               over 5 years of expertise in designing and implementing robust applications across Banking, Healthcare, and Consulting industries. 
               Strong expertise in distributed systems using Apache Kafka, Redis, and modern cloud platforms such as Microsoft Azure and 
               AWS to build resilient, high-performance systems. Proven track record in delivering scalable solutions that 
-              balance technical excellence with business objectives"
+              balance technical excellence with business objectives."
               size="1rem" weight="300" color="whitesmoke" />
             <a href="https://docs.google.com/document/d/1AEvk3TsbLan5P6vuF1yGmkf5-JJPufX9gzs9R52xJBU/edit?usp=sharing" target="_blank">
               <Button text="Download Resume" />
