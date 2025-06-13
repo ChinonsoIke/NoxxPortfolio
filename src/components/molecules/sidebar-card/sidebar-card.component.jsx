1197b6aad8e966429f598122e83605c451aba873
Chinonso Ikewelugo <chinonsoikewelugo@gmail.com>
2025-06-13 02:19:15 -0500

    change image

--- src/components/molecules/sidebar-card/sidebar-card.component.jsx
+++ src/components/molecules/sidebar-card/sidebar-card.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from '../../atoms/image/image.component'
 import { CardStyles } from './sidebar-card.styles'
-import Noxx from '../../../assets/images/noxx.jpg'
+import noxx2 from '../../../assets/images/noxx.jpg'
 import Text from '../../atoms/text/text.component'
 
 const SidebarCard = () => {
   return (
     <CardStyles>
-        <Image src={Noxx} height="5rem" width="5rem" borderRadius="50%" />
+        <Image src={noxx2} height="5rem" width="5rem" borderRadius="50%" />
         <Text text="CHINONSO IKEWELUGO" weight="600" />
         <Text text="Full Stack Software Engineer" />
     </CardStyles>
